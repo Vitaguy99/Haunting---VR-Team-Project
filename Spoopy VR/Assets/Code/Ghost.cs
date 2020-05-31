db09c625f2efc809a2258b2e75e7cd37db37eb2b
Luke <lukewellsdesign@gmail.com>
2020-05-31 20:39:47 +1000

    Attempt at ghost behavior

---
+++ Spoopy VR/Assets/Code/Ghost.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public GameObject ghost;
+    public GameObject Player;
+    public float Speed;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards (transform.position, Player.transform.position, Speed * Time.deltaTime); 
+    }
+
+    private void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.gameObject.tag == "Looking")
+        {
+            Speed = 0f;
+        }
+    }
+
+    private void OnTriggerExit(Collider Coll)
+    {
+        if (Coll.gameObject.tag == "Looking")
+        {
+            Speed = 5f;
+        }
+    }
+}
