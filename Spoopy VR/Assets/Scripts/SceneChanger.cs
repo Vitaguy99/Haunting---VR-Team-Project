b3ca7abacc889e6dfe4059aecd82e964f4177f67
Luke <lukewellsdesign@gmail.com>
2020-06-03 10:30:33 +1000

    Added UI to Menu Grey-box

---
+++ Spoopy VR/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneIndex);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Link(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
